ab254856ec0f559273f7134583881b4c9f059edb
root <root@localhost.localdomain>
2015-09-25 17:35:04 -0400

    sequencial

---
+++ Sequencial/seq.c
@@ -0,0 +1,44 @@
+#include <openssl/x509.h>
+#include <openssl/evp.h>
+#include <openssl/hmac.h>
+#include <omp.h>
+
+ #define IC 4096
+
+char* itoa(int val){
+	
+	static char buf[32] = {0};
+	
+	int i = 30;
+	
+	for(; val && i ; --i, val /= 10)
+		buf[i] = "0123456789abcdef"[val % 10];
+	
+	return &buf[i+1];
+}
+	
+int print_hex(unsigned char *buf, int len)
+{
+    for(int i=0;i<len;i++)
+        printf("%02x",buf[i]);
+
+    printf("\n");
+
+    return(0);
+}
+
+int main()
+{
+    char *pass = itoa(5478214);
+    char *salt = "test";
+    unsigned char buf[33];
+
+    #pragma omp parallel for private(pass)
+    for (int i = 0; i < 999; i++)
+    {
+    	pass = itoa(i);
+    	PKCS5_PBKDF2_HMAC_SHA1(pass, 8, salt, 4, 4096, 32, buf);
+    }
+
+    return 0;
+}
