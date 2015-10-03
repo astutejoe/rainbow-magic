936cb789f4b84236993b15bdb39e1ff5ba58a38a
Gabriel Garcia <gabrielaggarcia@hotmail.com>
2015-10-02 21:35:19 -0400

    righting wrongs

--- src/sha1.h
+++ src/sha1.h
@@ -11,11 +11,4 @@ typedef struct {
 } blk_SHA_CTX;
 
 void sha1(const void*, unsigned long, unsigned char[20]);
-void blk_SHA1_Init(blk_SHA_CTX *ctx);
 void blk_SHA1_Update(blk_SHA_CTX *ctx, const void *dataIn, unsigned long len);
-void blk_SHA1_Final(unsigned char hashout[20], blk_SHA_CTX *ctx);
-
-#define git_SHA_CTX	blk_SHA_CTX
-#define git_SHA1_Init	blk_SHA1_Init
-#define git_SHA1_Update	blk_SHA1_Update
-#define git_SHA1_Final	blk_SHA1_Final
